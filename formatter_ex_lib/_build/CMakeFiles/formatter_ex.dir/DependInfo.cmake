9056a7e67323d61fbb32bd3f667c381088a13463
theenqe <99952139+theenqe@users.noreply.github.com>
2022-10-23 17:01:03 +0300

    Add files via upload

---
+++ formatter_ex_lib/_build/CMakeFiles/formatter_ex.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/inspired/T_lb3/formatter_ex_lib/formatter_ex.cpp" "/home/inspired/T_lb3/formatter_ex_lib/_build/CMakeFiles/formatter_ex.dir/formatter_ex.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "/home/inspired/T_lb3/formatter_lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/inspired/T_lb3/formatter_ex_lib/_build/formatter_lib/CMakeFiles/formatter.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ formatter_ex_lib/_build/CMakeFiles/formatter_ex.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/formatter_ex.dir/formatter_ex.cpp.o"
+  "libformatter_ex.a"
+  "libformatter_ex.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/formatter_ex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
