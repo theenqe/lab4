eec28dfcaa235965538f4e8d79727b82c40322b6
theenqe <99952139+theenqe@users.noreply.github.com>
2022-10-23 17:03:14 +0300

    Add files via upload

---
+++ hello_world_application/_build/formatter_ex/formatter_lib/CMakeFiles/formatter.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/inspired/T_lb3/formatter_lib/formatter.cpp" "/home/inspired/T_lb3/hello_world_application/_build/formatter_ex/formatter_lib/CMakeFiles/formatter.dir/formatter.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/inspired/T_lb3/formatter_lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
